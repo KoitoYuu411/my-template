2f0391ddbfd54507fb077b285144a0359eeba8f9
KoitoYuu411 <67617392+KoitoYuu411@users.noreply.github.com>
2021-09-12 22:46:22 +0800

    Update all.cpp

--- all.cpp
+++ all.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define debug(...) cout << #__VA_ARGS__ << "\t" << forward_as_tuple(__VA_ARGS__)/ebra << endl;
 #define ALL(...) begin(__VA_ARGS__), end(__VA_ARGS__)
 #define RALL(...) rbegin(__VA_ARGS__), rend(__VA_ARGS__)
@@ -14,7 +16,12 @@ using namespace std;
 using ll = long long int;
 using ull = unsigned long long int;
 
-
+namespace detail {
+using namespace __gnu_pbds;
+template<class T, class V = null_type, class C = less<>>
+using order_tree = tree<T, V, C, rb_tree_tag, tree_order_statistics_node_update>;
+}
+using detail::order_tree;
 template<int N>
 constexpr auto SieveOfEuler_() {
     array<int, N + 1> prime {};
@@ -32,16 +39,12 @@ constexpr auto SieveOfEuler_() {
     }
     return pair {prime, pos};
 };
-
-
-
 inline namespace type_traits {
 
 template<class T> struct type_identity { using type = T; };
 template<class T> using type_identity_t = typename type_identity<T>::type;
     
-template<class T, class... Args>
-concept is_any_of = ( is_same_v<T, Args> || ... );
+template<class T, class... Args> concept is_any_of = ( is_same_v<T, Args> || ... );
 
 template<class T, class = void> INLINE_BOOL is_tuple_like = false;
 
@@ -56,6 +59,155 @@ inline namespace concepts {
 inline namespace integer {
 template<class T> constexpr make_unsigned_t<T> to_unsigned(T t) noexcept { return t; }
 template<class T> constexpr make_signed_t<T> to_signed(T t) noexcept { return t; }
+
+template <class T> constexpr T __rotl(T x, int s) noexcept {
+    constexpr auto _Nd = numeric_limits<T>::digits;
+    const int r = s % _Nd;
+    if (r == 0) return x;
+    else if (r > 0) return (x << r) | (x >> ((_Nd - r) % _Nd));
+    else return (x >> -r) | (x << ((_Nd + r) % _Nd)); // rotr(x, -r)
+}
+template <class T> constexpr T __rotr(T x, int s) noexcept {
+    constexpr auto _Nd = numeric_limits<T>::digits;
+    const int r = s % _Nd;
+    if (r == 0) return x;
+    else if (r > 0) return (x >> r) | (x << ((_Nd - r) % _Nd));
+    else return (x << -r) | (x >> ((_Nd + r) % _Nd)); // rotl(x, -r)
+}
+template <class T> constexpr int __countl_zero(T x) noexcept {
+    constexpr auto _Nd = numeric_limits<T>::digits;
+    if (x == 0) return _Nd;
+    constexpr auto _Nd_ull = numeric_limits<unsigned long long>::digits;
+    constexpr auto _Nd_ul = numeric_limits<unsigned long>::digits;
+    constexpr auto _Nd_u = numeric_limits<unsigned>::digits;
+    if constexpr (_Nd <= _Nd_u) {
+        constexpr int diff = _Nd_u - _Nd;
+        return __builtin_clz(x) - diff;
+    } else if constexpr (_Nd <= _Nd_ul) {
+        constexpr int diff = _Nd_ul - _Nd;
+        return __builtin_clzl(x) - diff;
+    } else if constexpr (_Nd <= _Nd_ull) {
+        constexpr int diff = _Nd_ull - _Nd;
+        return __builtin_clzll(x) - diff;
+    } else { //(_Nd > _Nd_ull)
+        static_assert(_Nd <= (2 * _Nd_ull), "Maximum supported integer size is 128-bit");
+        unsigned long long high = x >> _Nd_ull;
+        if (high != 0) {
+            constexpr int diff = (2 * _Nd_ull) - _Nd;
+            return __builtin_clzll(high) - diff;
+        }
+        constexpr auto __max_ull = numeric_limits<unsigned long long>::max();
+        unsigned long long __low = x & __max_ull;
+        return (_Nd - _Nd_ull) + __builtin_clzll(__low);
+    }
+}
+template <class T> constexpr int __countl_one(T x) noexcept {
+    if (x == numeric_limits<T>::max()) return numeric_limits<T>::digits;
+    return __countl_zero<T>((T)~x);
+}
+
+template <class T> constexpr int __countr_zero(T x) noexcept {
+    constexpr auto _Nd = numeric_limits<T>::digits;
+    if (x == 0) return _Nd;
+    constexpr auto _Nd_ull = numeric_limits<unsigned long long>::digits;
+    constexpr auto _Nd_ul = numeric_limits<unsigned long>::digits;
+    constexpr auto _Nd_u = numeric_limits<unsigned>::digits;
+    if constexpr (_Nd <= _Nd_u) return __builtin_ctz(x);
+    else if constexpr (_Nd <= _Nd_ul) return __builtin_ctzl(x);
+    else if constexpr (_Nd <= _Nd_ull) return __builtin_ctzll(x);
+    else // (_Nd > _Nd_ull)
+    {
+        static_assert(_Nd <= (2 * _Nd_ull), "Maximum supported integer size is 128-bit");
+        constexpr auto __max_ull = numeric_limits<unsigned long long>::max();
+        unsigned long long __low = x & __max_ull;
+        if (__low != 0) return __builtin_ctzll(__low);
+        unsigned long long high = x >> _Nd_ull;
+        return __builtin_ctzll(high) + _Nd_ull;
+    }
+}
+template <class T> constexpr int __countr_one(T x) noexcept {
+    if (x == numeric_limits<T>::max()) return numeric_limits<T>::digits;
+    return __countr_zero((T)~x);
+}
+template <class T> constexpr int __popcount(T x) noexcept {
+    constexpr auto _Nd = numeric_limits<T>::digits;
+    if (x == 0) return 0;
+    constexpr auto _Nd_ull = numeric_limits<unsigned long long>::digits;
+    constexpr auto _Nd_ul = numeric_limits<unsigned long>::digits;
+    constexpr auto _Nd_u = numeric_limits<unsigned>::digits;
+    if constexpr (_Nd <= _Nd_u) return __builtin_popcount(x);
+    else if constexpr (_Nd <= _Nd_ul) return __builtin_popcountl(x);
+    else if constexpr (_Nd <= _Nd_ull) return __builtin_popcountll(x);
+    else { // (_Nd > _Nd_ull)
+        static_assert(_Nd <= (2 * _Nd_ull), "Maximum supported integer size is 128-bit");
+        constexpr auto __max_ull = numeric_limits<unsigned long long>::max();
+        unsigned long long __low = x & __max_ull;
+        unsigned long long high = x >> _Nd_ull;
+        return __builtin_popcountll(__low) + __builtin_popcountll(high);
+    }
+}
+template <class T> constexpr bool __has_single_bit(T x) noexcept { return __popcount(x) == 1; }
+template <class T> constexpr T __bit_ceil(T x) noexcept {
+    constexpr auto _Nd = numeric_limits<T>::digits;
+    if (x == 0 || x == 1) return 1;
+    auto shift_exponent = _Nd - __countl_zero((T)(x - 1u));
+    using __promoted_type = decltype(x << 1);
+    if constexpr (!is_same<__promoted_type, T>::value) {
+        const int __extra_exp = sizeof(__promoted_type) / sizeof(T) / 2;
+        shift_exponent |= (shift_exponent & _Nd) << __extra_exp;
+    }
+    return (T)1u << shift_exponent;
+}
+template <class T> constexpr T __bit_floor(T x) noexcept {
+    constexpr auto _Nd = numeric_limits<T>::digits;
+    if (x == 0) return 0;
+    return (T)1u << (_Nd - __countl_zero((T)(x >> 1)));
+}
+template <class T> constexpr T __bit_width(T x) noexcept {
+    constexpr auto _Nd = numeric_limits<T>::digits;
+    return _Nd - __countl_zero(x);
+}
+template <class T, class U = T> using bit_require = enable_if_t<is_integral_v<T> && is_unsigned_v<T>, U>;
+
+//[bit.cast]
+template <class To, class From>
+enable_if_t<(sizeof(To) == sizeof(From)) && is_trivially_copyable<From>::value && is_trivial<To>::value, To>
+bit_cast(const From &src) noexcept {
+    To dst;
+    memcpy(&dst, &src, sizeof(To));
+    return dst;
+}
+// [bit.rot], rotating
+/// Rotate `x` to the left by `s` bits.
+template <class T> constexpr bit_require<T> rotl(T x, int s) noexcept { return __rotl(x, s); }
+/// Rotate `x` to the right by `s` bits.
+template <class T> constexpr bit_require<T> rotr(T x, int s) noexcept { return __rotr(x, s); }
+
+// [bit.count], counting
+/// The number of contiguous zero bits, starting from the highest bit.
+template <class T> constexpr bit_require<T, int> countl_zero(T x) noexcept { return __countl_zero(x); }
+/// The number of contiguous one bits, starting from the highest bit.
+template <class T> constexpr bit_require<T, int> countl_one(T x) noexcept { return __countl_one(x); } 
+/// The number of contiguous zero bits, starting from the lowest bit.
+template <class T> constexpr bit_require<T, int> countr_zero(T x) noexcept { return __countr_zero(x); }
+/// The number of contiguous one bits, starting from the lowest bit.
+template <class T> constexpr bit_require<T, int> countr_one(T x) noexcept { return __countr_one(x); }
+/// The number of bits set in `x`.
+template <class T> constexpr bit_require<T, int> popcount(T x) noexcept { return __popcount(x); }
+
+// [bit.pow.two], integral powers of 2
+/// True if `x` is a power of two, false otherwise.
+template <class T> constexpr bit_require<T, bool> has_single_bit(T x) noexcept { return __has_single_bit(x); }
+/// The smallest power-of-two not less than `x`.
+template <class T> constexpr bit_require<T> bit_ceil(T x) noexcept { return __bit_ceil(x); }
+/// The largest power-of-two not greater than `x`.
+template <class T> constexpr bit_require<T> bit_floor(T x) noexcept { return __bit_floor(x); }
+/// The smallest integer greater than the base-2 logarithm of `x`.
+template <class T> constexpr bit_require<T> bit_width(T x) noexcept { return __bit_width(x); }
+
+/// Byte order
+enum class endian { little = __ORDER_LITTLE_ENDIAN__, big = __ORDER_BIG_ENDIAN__, native = __BYTE_ORDER__ };
+
 }
 inline namespace algo {
 
@@ -289,9 +441,10 @@ struct subset_view {
     private:
         T t, cur;
     };
-    subset_view(T t) noexcept: t(t) {}
-    iterator begin() const noexcept { return { t };  }
-    default_sentinel_t end() const noexcept { return {}; }
+    constexpr subset_view(T t) noexcept: t(t) {}
+    constexpr iterator begin() const noexcept { return { t };  }
+    constexpr default_sentinel_t end() const noexcept { return {}; }
+    auto constexpr size() const noexcept { return to_unsigned(T{1})  << __popcount(t);  }
 private:
     T t;
 };
@@ -304,6 +457,44 @@ struct subset_fn {
 };
 inline constexpr subset_fn subset {};
 } // namespace views
+template<class T> struct decompose_view {
+    struct iterator {
+        using iterator_category = forward_iterator_tag;
+        using value_type = T;
+        using reference = T;
+        using pointer = void;
+        using difference_type = make_signed_t<decltype(T() - T())>;
+        void satisfy() noexcept { while (x % i != 0) { if (i * i > x) { i = x; break; } ++i; } }
+        constexpr iterator(T x) noexcept : i(2), x(x) { satisfy(); }
+        constexpr iterator& operator++() noexcept { x /= i; satisfy(); return *this; }
+        constexpr iterator operator++(int) noexcept { auto cp = *this; ++*this; return cp; }
+        constexpr T operator*() const noexcept { return i; }
+        constexpr friend bool operator==(const iterator& i, const iterator& j) noexcept { return i.x == j.x && i.i == j.i; }
+        constexpr friend bool operator!=(const iterator& i, const iterator& j) noexcept { return !(i==j); }
+        constexpr friend bool operator==(const iterator& i, default_sentinel_t) noexcept { return i.x == 1; }
+        constexpr friend bool operator==(default_sentinel_t, const iterator& i) noexcept { return i.x == 1; }
+        constexpr friend bool operator!=(const iterator& i, default_sentinel_t) noexcept { return i.x != 1; }
+        constexpr friend bool operator!=(default_sentinel_t, const iterator& i) noexcept { return i.x != 1; }
+    private:
+        T x, i;
+    };
+    constexpr decompose_view(T t) noexcept: t(t) {}
+    constexpr iterator begin() const noexcept { return { t };  }
+    constexpr default_sentinel_t end() const noexcept { return {}; }
+private:
+    T t;
+};
+
+namespace views {
+struct decompose_fn { //decompose_view
+    template<class T, enable_if_t<is_integral_v<T>,int> =0>
+    auto constexpr operator()(T t) const noexcept { return decompose_view { t }; }
+    template<class T, enable_if_t<is_integral_v<T>,int> =0>
+    friend auto constexpr operator|(T t, decompose_fn f) noexcept { return f(t); }
+};
+inline constexpr decompose_fn decompose;
+} // namespace views
+
 } // namespace ranges
 inline namespace print {
 template<class T> struct brackets { T left; T right; };
@@ -723,13 +914,14 @@ constexpr int dir8[][2] { {0,1}, {1,0}, {0,-1}, {-1,0}, {1,1}, {-1,1}, {1,-1}, {
 constexpr auto valid = [](auto m, auto n) { return [=](size_t x, size_t y) { return x < m && y < n; }; };
 }
 inline namespace init {
-
+#ifdef __cpp_lib_memory_resource
 inline constexpr auto set_pmr = [] {
     static byte buffer [1 << 30];
     static auto pool = pmr::monotonic_buffer_resource { data(buffer), size(buffer) };
     set_default_resource(&pool);
     return 0;
 };
+#endif
 
 inline constexpr auto set_fast_io = [] { 
     ios::sync_with_stdio(false);
@@ -754,6 +946,8 @@ inline namespace simplify {
     inline constexpr ranges::views::iota_fn range {};
     inline constexpr ranges::views::zip_fn zp {};
     inline constexpr ranges::views::enumerate_fn en {};
+    inline constexpr ranges::views::subset_fn subset{};
     #define Yc Y_combinator
 } // namespace simplify
+
 
