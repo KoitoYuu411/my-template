b7bc34b628760a46c97f2539370d6f53cc93ade2
KoitoYuu411 <67617392+KoitoYuu411@users.noreply.github.com>
2021-10-19 20:33:38 +0800

    concat_test_case

--- test_case.cpp
+++ test_case.cpp
@@ -41,7 +41,38 @@ auto z=y|Min(x,less{},[&](auto&&i)RET(nums[i])); // equals to Min(y,x,less{},Lam
 debug(z)
 y|=Min(x);//equals to y=y|Min(x) -> y=Min(y,x)
 debug(y)
-
   
   
-  
+//[Concat]
+template<class R,class T>
+auto test_ra_concat(initializer_list<T> lst, R&&c) {
+    static_assert(ranges::ra_rg<R>);
+    auto [f,l]=c;
+    auto iters = views::iota(f,l);
+    for (auto [i,it] : iters|Enum) {
+        assert(it==f+i);
+        assert(it-i==f);
+        assert(f+i==it);
+        assert(it-f==i);
+        assert(f[i]==*it);
+    }
+    for (auto [i,it] : iters|Enum)for (auto [j,jt] : iters|Enum) {
+        assert(j-i==jt-it);
+        assert(it + (j-i) == jt);
+        assert(jt - (j-i) == it);
+    }
+    assert(equal(ALL(lst), f, l));
+};
+auto test_concat() {
+    auto vec = vector {1,2,3};
+    auto lst = {4,5,6};
+    auto sg = single(7);
+    auto empty = vector<int>{};
+    auto nums=ranges::iota_view<int,int>{8,10};
+    auto c=views::concat(vec,lst,sg,empty,nums);
+    test_ra_concat({1,2,3,4,5,6,7,8,9},c);
+    using R=decltype(c);
+    IS_SAME(ranges::rr_t<R>,int)
+    IS_SAME(ranges::rv_t<R>,int)
+    IS_SAME(ranges::rd_t<R>,long)
+}
